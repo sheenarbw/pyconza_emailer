43d3a7c3ad9e7b4abe0d67ce3cd53fd27c88a49b
Sheena <sheena.oconnell@gmail.com>
2021-06-29 18:59:09 +0200

    can send email with images

--- utils.py
+++ utils.py
@@ -3,60 +3,85 @@ from email.message import EmailMessage
 from email.utils import formatdate
 from config import SMTP_HOST, SMTP_PASSWORD, SMTP_USERNAME
 
-# import markdown
 
-
-def send_email(to_email, from_email, text):
+def send_email(message):
     context = ssl.create_default_context()
-
     PORT = 587
     server = smtplib.SMTP(SMTP_HOST, PORT)
     server.ehlo()
     server.starttls(context=context)
     server.ehlo()
     server.login(SMTP_USERNAME, SMTP_PASSWORD)
-
-    message = EmailMessage()
-    message.set_content(text)
-    message["Subject"] = f"test"
-    message["From"] = from_email
-    message["To"] = to_email
-    message["Date"] = formatdate(localtime=True)
     server.send_message(message)
     server.quit()
 
 
-# with open("templates/base_sponsorship_email.md") as f:
-#     message = f.read()
+def send_text_email(to_email, from_email, subject, text):
+    message = EmailMessage()
+    message.set_content(text)
+    message["Subject"] = subject
+    message["From"] = from_email
+    message["To"] = to_email
+    message["Date"] = formatdate(localtime=True)
+    send_email(message)
 
 
-# html = markdown.markdown(message)
+def send_html_email(to_email, from_email, subject, html):
+    import html2text
+    from email.utils import make_msgid
+    from email.mime.text import MIMEText
+    from email.mime.multipart import MIMEMultipart
 
-# send_email(
-#     to_email="sheena.oconnell@umuzi.org",
-#     from_email="sponsorship@za.pycon.org",
-#     text=message,
-#     html=html,
-# )
+    message = EmailMessage()
+    message["Subject"] = subject
+    message["From"] = from_email
+    message["To"] = to_email
+
+    h = html2text.HTML2Text()
+    h.ignore_emphasis = True
+    h.ignore_images = True
+    h.ignore_tables = True
+
+    text = h.handle(html)
+
+    logo_cid = make_msgid()
+
+    html = html.replace("{logo_cid}", logo_cid[1:-1])
+
+    message.set_content(text)
+    message.add_alternative(html, subtype="html")
+
+    with open("images/logo.png", "rb") as img:
+        message.get_payload()[1].add_related(img.read(), "image", "png", cid=logo_cid)
+
+    send_email(message)
 
 
-def render_template(name):
-    from mako.template import Template
+def render_template(name, render_kwargs):
     from mako.lookup import TemplateLookup
 
     lookup = TemplateLookup(directories=["templates"])
     template = lookup.get_template(name)
-    return template.render()
+    return template.render(**render_kwargs)
 
 
-def render_to_file(name):
+def render_to_file(name, render_kwargs):
     """just for testing purposes"""
     from pathlib import Path
 
-    result = render_template(name)
+    result = render_template(name, render_kwargs)
     with open(Path("gitignore") / name, "w") as f:
         f.write(result)
 
 
-render_to_file("base.html")
-render_to_file("first_time_sponsor_email.html")
+# html = render_template(
+#     "email_previous_sponsor.html",
+#     {"companys": "Your company's", "title": "PyConZA 2021 is looking for sponsors"},
+# )
+
+# send_html_email(
+#     to_email="sheena.oconnell@umuzi.org",
+#     from_email="sponsorship@za.pycon.org",
+#     html=html,
+#     subject="PyconZa call for sponsors",
+# )
